fd8da6a06dfcc55f5267ca2bb7775161a87718e9
shay <shay5nis@gmail.com>
2025-01-09 23:09:49 +0200

    add new exerciseDetailsFragment

---
+++ app/src/main/java/com/example/gymnastlink/ui/fragments/ExerciseDetailsFragment.kt
@@ -0,0 +1,69 @@
+package com.example.gymnastlink.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.example.gymnastlink.R
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ExerciseDetailsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ExerciseDetailsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val cardView = view?.findViewById<MaterialCardView>(R.id.card)
+        cardView?.setBackgroundResource(R.drawable.gradient_background_blue)
+        val fab = view?.findViewById<FloatingActionButton>(R.id.exercise_details_save_fab)
+        fab?.background = ContextCompat.getDrawable(requireContext(), R.drawable.gradient_background_blue)
+
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exercise_details, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ExerciseDetailsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ExerciseDetailsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
