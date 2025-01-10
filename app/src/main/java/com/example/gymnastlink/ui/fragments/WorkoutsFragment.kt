49e6dc26c0a41849bb6906653c4783a72c3a2eef
Daniel Pasker <danielpasker123@gmail.com>
2025-01-10 18:29:50 +0200

    workouts fragment refinements

--- app/src/main/java/com/example/gymnastlink/ui/fragments/WorkoutsFragment.kt
+++ app/src/main/java/com/example/gymnastlink/ui/fragments/WorkoutsFragment.kt
@@ -1,15 +1,13 @@
 package com.example.gymnastlink.ui.fragments
 
-import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Bundle
-import android.util.TypedValue
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gymnastlink.R
@@ -17,16 +15,19 @@ import com.example.gymnastlink.model.BodyMuscle
 import com.example.gymnastlink.model.ExerciseItem
 import com.example.gymnastlink.ui.MainActivity
 import com.example.gymnastlink.ui.adapters.ExerciseAdapter
-import java.lang.reflect.Field
 
 class WorkoutsFragment : Fragment() {
 
-    private lateinit var searchView: SearchView
-    private lateinit var exerciseRecyclerView: RecyclerView
-    private lateinit var adapter: ExerciseAdapter
+    private lateinit var workoutSearchEditText: EditText
+    private lateinit var searchResultsLayout: View
+    private lateinit var searchResultsRecyclerView: RecyclerView
+    private lateinit var myPlanRecyclerView: RecyclerView
+    private lateinit var searchResultAdapter: ExerciseAdapter
+    private lateinit var myPlanAdapter: ExerciseAdapter
 
     companion object {
-        val exerciseList = mutableListOf<Any>()
+        val searchResults = mutableListOf<ExerciseItem>()
+        val myPlan = mutableListOf<ExerciseItem>()
     }
 
     override fun onCreateView(
@@ -36,69 +37,61 @@ class WorkoutsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_workouts, container, false)
     }
 
-    @SuppressLint("DiscouragedPrivateApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as? MainActivity)?.setFragmentTitle(getString(R.string.workouts))
 
-        searchView = view.findViewById(R.id.workout_search)
-        changeSearchViewHintTextDisplay()
+        workoutSearchEditText = view.findViewById(R.id.workout_search)
+        searchResultsLayout = view.findViewById(R.id.search_results_layout)
+        searchResultsRecyclerView = view.findViewById(R.id.search_results_recyclerview)
+        myPlanRecyclerView = view.findViewById(R.id.my_plan_recyclerview)
 
-        exerciseRecyclerView = view.findViewById(R.id.exercises_recyclerview)
-        exerciseRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-        adapter = ExerciseAdapter(exerciseList)
-        exerciseRecyclerView.adapter = adapter
-        // TODO: needs to be removed, get data from real source
-        if (exerciseList.isEmpty()) {
-            loadSearchedExercises()
-            loadMyExercises()
-        }
+        // TODO: needs to be changed, get data from db
+        val dummyPlan = listOf(
+            ExerciseItem("Dumbbells", BodyMuscle.BICEPS, null),
+            ExerciseItem("Arnold Press", BodyMuscle.DELTOID, null),
+            ExerciseItem("Wrist Curl", BodyMuscle.FOREARMS, null)
+        )
+        myPlan.addAll(dummyPlan)
+
+        myPlanAdapter = ExerciseAdapter(myPlan)
+        myPlanRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        myPlanRecyclerView.adapter = myPlanAdapter
+
+        searchResultAdapter = ExerciseAdapter(searchResults)
+        searchResultsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        searchResultsRecyclerView.adapter = searchResultAdapter
+
+        workoutSearchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    searchResultsLayout.visibility = View.GONE
+                } else {
+                    searchResultsLayout.visibility = View.VISIBLE
+                    loadSearchResults()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
     override fun onResume() {
         super.onResume()
-        adapter.notifyDataSetChanged()
+        myPlanAdapter.notifyDataSetChanged()
+        searchResultAdapter.notifyDataSetChanged()
     }
 
-    private fun changeSearchViewHintTextDisplay() {
-        try {
-            val searchEditTextField: Field = SearchView::class.java.getDeclaredField("mSearchSrcTextView")
-            searchEditTextField.isAccessible = true
-            val searchEditText: EditText = searchEditTextField.get(searchView) as EditText
-            searchEditText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f)
-            searchEditText.setHintTextColor(Color.WHITE)
-        } catch (e: NoSuchFieldException) {
-            e.printStackTrace()
-        } catch (e: IllegalAccessException) {
-            e.printStackTrace()
-        }
-    }
     // TODO: needs to be changed, get data from real source
-    private fun loadSearchedExercises(){
-        exerciseList.add(getString(R.string.recently_searched_header))
-        val recentlySearchedIndex = exerciseList.indexOf(getString(R.string.recently_searched_header))
-        if (recentlySearchedIndex != -1) {
-            val exercises = listOf(
-                ExerciseItem("Dumbbells", BodyMuscle.BICEPS, null),
-                ExerciseItem("Arnold Press", BodyMuscle.DELTOID, null),
-                ExerciseItem("Wrist Curl", BodyMuscle.FOREARMS, null)
-            )
-            exerciseList.addAll(recentlySearchedIndex + 1, exercises)
-            adapter.notifyItemRangeInserted(recentlySearchedIndex + 1, exercises.size)
-        }
-    }
-    // TODO: needs to be changed, get data from real source
-    private fun loadMyExercises(){
-        exerciseList.add(getString(R.string.my_plan_header))
-        val myPlanIndex = exerciseList.indexOf(getString(R.string.my_plan_header))
-        if (myPlanIndex != -1) {
-            val exercises1 = listOf(
-                ExerciseItem("Lateral Step-Up", BodyMuscle.GLUTEUS_MAXIMUS,null),
-                ExerciseItem("Squats", BodyMuscle.QUADRICEPS, null),
-                ExerciseItem("Hammer Curl", BodyMuscle.FOREARMS, null)
-            )
-            exerciseList.addAll(myPlanIndex + 1, exercises1)
-            adapter.notifyItemRangeInserted(myPlanIndex + 1, exercises1.size)
-        }
+    private fun loadSearchResults() {
+        val dummyResults = listOf(
+            ExerciseItem("Dumbbells", BodyMuscle.BICEPS, null),
+            ExerciseItem("Arnold Press", BodyMuscle.DELTOID, null),
+            ExerciseItem("Wrist Curl", BodyMuscle.FOREARMS, null)
+        )
+        searchResults.addAll(dummyResults)
+        searchResultAdapter.notifyDataSetChanged()
     }
 }
