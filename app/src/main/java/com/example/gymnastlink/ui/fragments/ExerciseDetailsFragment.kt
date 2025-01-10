835a96ba9a72dd158e360a5b38df5965f25930fb
shay <shay5nis@gmail.com>
2025-01-10 21:08:17 +0200

    finish exercise details fragment ui

--- app/src/main/java/com/example/gymnastlink/ui/fragments/ExerciseDetailsFragment.kt
+++ app/src/main/java/com/example/gymnastlink/ui/fragments/ExerciseDetailsFragment.kt
@@ -1,69 +1,81 @@
 package com.example.gymnastlink.ui.fragments
 
+import ExerciseItem
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.gymnastlink.R
+import com.example.gymnastlink.ui.MainActivity
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ExerciseDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ExerciseDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val cardView = view?.findViewById<MaterialCardView>(R.id.card)
-        cardView?.setBackgroundResource(R.drawable.gradient_background_blue)
-        val fab = view?.findViewById<FloatingActionButton>(R.id.exercise_details_save_fab)
-        fab?.background = ContextCompat.getDrawable(requireContext(), R.drawable.gradient_background_blue)
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var exerciseName: TextView
+    private lateinit var muscleName: TextView
+    private lateinit var equipmentName: TextView
+    private lateinit var description: TextView
+    private lateinit var setsNumber: TextView
+    private lateinit var repsNumber: TextView
+    private lateinit var img: ImageView
+    private var position: Int = 0
+    private lateinit var exerciseItem: ExerciseItem
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise_details, container, false)
+        val view = inflater.inflate(R.layout.fragment_exercise_details, container, false)
+
+        arguments?.let {
+            position = it.getInt("position")
+            exerciseItem = it.getParcelable("exerciseItem")!!
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExerciseDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExerciseDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? MainActivity)?.setFragmentTitle(getString(R.string.exercise_details))
+
+        view.findViewById<MaterialCardView>(R.id.exercise_details_card).setBackgroundResource(
+            R.drawable.gradient_background_blue)
+
+        view.findViewById<FloatingActionButton>(R.id.exercise_details_cancel_fab).apply {
+            setOnClickListener {
+                findNavController().navigateUp()
             }
+        }
+
+        exerciseName = view.findViewById(R.id.exercuse_details_name_textview)
+        muscleName = view.findViewById(R.id.exercise_details_muscle_name)
+        equipmentName = view.findViewById(R.id.exercise_details_equipment_name)
+        description = view.findViewById(R.id.exercise_details_description)
+        setsNumber = view.findViewById(R.id.exercise_details_sets_number)
+        repsNumber = view.findViewById(R.id.exercise_details_reps_number)
+        img = view.findViewById(R.id.exercise_details_img_view)
+        displayExercise()
+    }
+
+    private fun displayExercise(){
+        exerciseName.text = exerciseItem.name
+        muscleName.setText("Muscle: ${exerciseItem.target.toString()}")
+        equipmentName.setText("Equipment: ${exerciseItem.equipment.toString()}")
+        description.setText(exerciseItem.instructions.joinToString(separator = "\n"))
+        setsNumber.setText("Sets: 3")
+        repsNumber.setText("Reps: 12")
+
+        Glide.with(requireContext())
+            .load(exerciseItem.gifUrl)
+            .placeholder(R.drawable.placeholder_image)
+            .into(img)
     }
 }
